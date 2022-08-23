db709c3856c4344d664109f37e2e11605f0b97bf
Jiawen Zhuo <jiawen.zhuo@uq.net.au>
2022-08-23 16:13:18 +1000

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-
-
 import { ThemeProvider } from 'styled-components'
 import { darkTheme } from '@yingxuanli/kanban-mangement-component'
 function App() {
-
   return (
     <ThemeProvider theme={darkTheme}>
       <h1>hello world</h1>
